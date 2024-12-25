02427b345194c74d6fb1046397ebb8e9ac129369
alba <aescribanoga@fpvirtualaragon.es>
2024-12-25 20:49:50 +0100

    Se modifica clase CCuenta.java

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,67 +1,90 @@
 package cuentas;
+
+/** author: Alba Escribano Gracia*/
 public class CCuenta {
 
-
+/** Atributos de la clase CCuenta*/
+	
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
 
+ /** Metodo GETTER para acceder al nombre del titular de la cuenta*/
+    
     public String getNombre() {
 		return nombre;
 	}
-
+ /** Metodo SETTER para modificar nombre del titular de la cuenta*/
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
+ /** Metodo GETTER para acceder al número de la cuenta*/
 	public String getCuenta() {
 		return cuenta;
 	}
-
+ /** Metodo SETTER para modificar número de la cuenta*/
 	public void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
-
+ /** Metodo GETTER para acceder al saldo de la cuenta*/
 	public double getSaldo() {
 		return saldo;
 	}
-
+ /** Metodo SETTER para modificar saldo de la cuenta*/
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
-
+ /** Metodo GETTER para acceder al tipo de interés de la cuenta*/
 	public double getTipoInterés() {
 		return tipoInterés;
 	}
-
+ /** Metodo SETTER para modificar tipo de interés de la cuenta*/
 	public void setTipoInterés(double tipoInterés) {
 		this.tipoInterés = tipoInterés;
 	}
-
+ /** Constructor por defecto de la clase CCuenta*/
+	
 	public CCuenta()
     {
     }
 
+/**
+ * Constructor para definir parámetros e inicializar valores de cuenta
+ * @param nom - para el nombre del titular
+ * @param cue - para nº de cuenta
+ * @param sal - para saldo inicial
+ * @param tipo - para tipo de interés
+ */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
         cuenta=cue;
         saldo=sal;
     }
-
+/**
+ * @return devuelve saldo actual que tiene la cuenta
+ */
     public double estado()
     {
         return saldo;
     }
-
+/**
+ * Si se ingresa una cantidad negativa lanza excepción
+ * @param cantidad - indicar cantidad a ingresar
+ * @throws Exception - excepción que lanza si importe es mayor que 0
+ */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
     }
-
+/**
+ * Si se retira importe negativo o saldo es insuficiente, lanza excepción
+ * @param cantidad - cantidad a retirar
+ * @throws Exception - excepción que lanza
+ */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
